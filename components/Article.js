1ee759b1b995a4b880d0e5de32533212faea5de8
rhammer35 <rhammer35@icloud.com>
2021-03-10 20:26:42 -0500

    added a couple more new article objects

--- components/Article.js
+++ components/Article.js
@@ -134,6 +134,50 @@ const data = [
     Take heed of the stormy weather
     And yes, there’s somethin' you can send back to me
     Spanish boots of Spanish leather`
+  },
+  {
+    title: 'Baird School Discplinary Hearing Transcript',
+    date: 'November 30, 1992',
+    firstParagraph: `Mr. Trask: I'm going to recommend to the disciplinary committee that you be expelled, Mr. Sims. You are a cover-up artist and you are a liar.
+
+    Frank Slade: But not a snitch.
+    
+    Trask: Excuse me?
+    
+    Slade: No, I don't think I will.
+    
+    Trask: Mr. Slade.
+    
+    Slade: This is such a crock of shit.
+    
+    Trask: Please watch your language, Mr. Slade. You are in the Baird School not a barracks. Mr. Sims, I will give you one final opportunity to speak up.
+    
+    Slade: Mr. Sims doesn't want it. He doesn't need to labeled: "Still worthy of being a 'Baird Man.'" What the hell is that? What is your motto here? "Boys, inform on your classmates, save your hide" -- anything short of that we're gonna burn you at the stake? Well, gentlemen, when the shit hits the fan some guys run and some guys stay. Here's Charlie facing the fire; and there's George hidin' in big Daddy's pocket. And what are you doin'? You're gonna reward George and destroy Charlie.`,
+
+    secondParagraph: `Trask: Are you finished, Mr. Slade?
+
+    Slade:No, I'm just gettin' warmed up. I don't know who went to this place, William Howard Taft, William Jennings Bryan, William Tell -- whoever. Their spirit is dead -- if they ever had one -- it's gone. You're building a rat ship here. A vessel for sea goin' snitches. And if you think you're preparing these minnows for manhood you better think again. Because I say you are killing the very spirit this institution proclaims it instills! What a sham. What kind of a show are you guys puttin' on here today. I mean, the only class in this act is sittin' next to me. And I'm here to tell ya this boy's soul is intact. It's non-negotiable. You know how I know? Someone here -- and I'm not gonna say who -- offered to buy it. Only Charlie here wasn't sellin'.
+  
+    Trask: Sir, you are out of order!
+  
+    Slade: Outta order? I'll show you outta order! You don't know what outta order is, Mr. Trask! I'd show you but I'm too old; I'm too tired; I'm too fuckin' blind. If I were the man I was five years ago I'd take a FLAME-THROWER to this place! Outta order. Who the hell you think you're talkin' to? I've been around, you know? There was a time I could see. And I have seen boys like these, younger than these, their arms torn out, their legs ripped off. But there isn't nothin' like the sight of an amputated spirit; there is no prosthetic for that. You think you're merely sendin' this splendid foot-soldier back home to Oregon with his tail between his legs, but I say you are executin' his SOUL!! And why?! Because he's not a Baird man! Baird men, ya hurt this boy, you're going to be Baird Bums, the lot of ya. And Harry, Jimmy, Trent, wherever you are out there, FUCK YOU, too!`,
+
+    thirdParagraph: `Trask: Stand down, Mr. Slade!
+
+    Slade: I'm not finished! As I came in here, I heard those words, "cradle of leadership." Well, when the bough breaks, the cradle will fall. And it has fallen here; it has fallen. Makers of men; creators of leaders; be careful what kind of leaders you're producin' here. I don't know if Charlie's silence here today is right or wrong.
+    
+    I'm not a judge or jury. But I can tell you this: he won't sell anybody out to buy his future!! And that, my friends, is called integrity! That's called courage! Now that's the stuff leaders should be made of. Now I have come to the crossroads in my life. I always knew what the right path was. Without exception, I knew. But I never took it. You know why? It was too damn hard. Now here's Charlie. He's come to the crossroads. He has chosen a path. It's the right path. It's a path made of principle -- that leads to character. Let him continue on his journey.
+    
+    You hold this boy's future in your hands, committee. It's a valuable future. Believe me. Don't destroy it! Protect it. Embrace it. It's gonna make ya proud one day -- I promise you.`
+  },
+  {
+    title: `Excerpt from 'Their Eyes Were Watching God' by Zora Neale Hurston`,
+    date: 'August 7, 1963',
+    firstParagraph: `Ships at a distance have every man’s wish on board. For some they come in with the tide. For others they sail forever on the horizon, never out of sight, never landing until the Watcher turns his eyes away in resignation, his dreams mocked to death by Time. That is the life of men.`,
+
+    secondParagraph: `Now, women forget all those things they don’t want to remember, and remember everything they don’t want to for-get. The dream is the truth. Then they act and do things accordingly`,
+
+    thirdParagraph: `So the beginning of this was a woman and she had come back from  burying  the  dead.  Not  the  dead  of  sick  and  ailing  with  friends at the pillow and the feet. She had come back from the sodden and the bloated; the sudden dead, their eyes flung wide open in judgment.`
   }
 ];
 
